e01dc9ac04530501d5ed489f5b5108ee2b52a72b
Gustavo Nielsen <gusanielsen@gmail.com>
2020-01-30 11:30:32 -0300

    Helpers

---
+++ DesafioUol/Helpers/AppConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DesafioUol.Helpers
+{
+    public static class AppConstants
+    {
+        public const string AppName = "TestUOL";
+        #region Prod or Development Enviroment
+        public const string Api = "https://darksky.net/dev";
+        public const string Apikey = "8cdfe5016e2c5632b2f1e060380b873a"; 
+        #endregion
+    }
+}
