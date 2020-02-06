66425f2e7a76df005e2e1b63d602dec1de210963
Gustavo Nielsen <gusanielsen@gmail.com>
2020-02-06 19:01:38 -0300

    So falta Fontes e Passar a cidade e Mudas as telas

--- TesteUol/ViewModels/TempoViewModel.cs
+++ TesteUol/ViewModels/TempoViewModel.cs
@@ -18,10 +18,11 @@ namespace TesteUol.ViewModels
 {
     public class TempoViewModel : ViewModelBase
     {
-       
        ForecastIORequest _ForecastIORequest;
         Unit Unit;
         Language Language;
+
+        #region Propriedades
 
         private ForecastIOResponse _tempo;
         public ForecastIOResponse Tempo
@@ -45,25 +46,6 @@ namespace TesteUol.ViewModels
             set { SetProperty(ref _daily, value); }
         }
 
-        private Minutely _minutely;
-        public Minutely Minutely
-        {
-            get { return _minutely; }
-            set { SetProperty(ref _minutely, value); }
-        }
-
-        private Hourly _houly;
-        public Hourly Hourly
-        {
-            get { return _houly; }
-            set { SetProperty(ref _houly, value); }
-        }
-        private string _cTemp;
-        public string CTemp
-        {
-            get { return _cTemp; }
-            set { SetProperty(ref _cTemp, value); }
-        }
 
         private DateTime _date;
         public DateTime Date
@@ -80,7 +62,7 @@ namespace TesteUol.ViewModels
             set { SetProperty(ref _city, value); }
         }
 
-       
+
         private ObservableCollection<DailyForecast> _dailyForecast;
         public ObservableCollection<DailyForecast> DailyForecast
         {
@@ -88,6 +70,7 @@ namespace TesteUol.ViewModels
             set { SetProperty(ref _dailyForecast, value); }
         }
 
+        #endregion
 
         #region Latitude e Loginditude da Geo Localização
         public double LatitudeGeolocalizacao { get; set; }
@@ -103,121 +86,52 @@ namespace TesteUol.ViewModels
 
         #endregion
 
-
         public TempoViewModel(INavigationService navigationService) : base(navigationService)
         {
             _taskInit = TaskInit();
         }
 
-    
         private async Task TaskInit()
         {
 
             await LoadGeolocation();
-           
             await LoadApi();
             await ApiDados();
             await LoadListDailyForecas();
         }
 
-        private async Task LoadApi()
-        {
-            
-         _ForecastIORequest = new ForecastIORequest(AppConstants.Apikey, LatitudeGeolocalizacao, LongitudeGeolocalizacao, Unit.si, Language.pt);
-            
-        }
+        #region ApiDados e Carregar Imagem Anida
 
         /// <summary>
-        /// Pegar Geo Localização Testando 
+        /// LoadApi
         /// </summary>
         /// <returns></returns>
-        private async Task LoadGeolocation()
+        private async Task LoadApi()
         {
-            try
-            {
-                var localizacao = await Geolocation.GetLastKnownLocationAsync();
-                if (localizacao != null)
-                {
-                    LatitudeGeolocalizacao = localizacao.Latitude;
-                    LongitudeGeolocalizacao  = localizacao.Longitude;
 
-                }
-            }
-            catch (FeatureNotSupportedException fnsEx)
-            {
-                // Recurso não suportado no device
-                var g =("Erro ", fnsEx.Message, "Ok");
-            }
-            catch (PermissionException pEx)
-            {
-                // Tratando erro de permissão
-                var g = ("Erro: ", pEx.Message, "Ok");
-            }
-            catch (Exception ex)
-            {
-                // Não foi possivel obter localização
-                var g = ("Erro : ", ex.Message, "Ok");
-            }
-
-
+            _ForecastIORequest = new ForecastIORequest(AppConstants.Apikey, LatitudeGeolocalizacao, LongitudeGeolocalizacao, Unit.si, Language.pt);
 
         }
-
-        private async Task LoadListDailyForecas()
-        {
-            try
-            {
-                // Array.Remove(Daily.data, 0);
-                Daily.data.RemoveAt(0);
-                foreach (var item in Daily.data)
-                {
-                    item.date =  Extensions.ToDateTime(item.time);
-                    item.celsiu = Extensions.ConvertCelsius(item.temperatureMax);
-
-                    // Pegar a Imagem de Acordo Com Summary
-                    // Explicando Para economizar tempo eu fiz deste jeito e logico que fosse MultiIdiomas faria de outra Maneira 
-                    if (item.summary.Contains("sol"))
-                    {
-                        item.icon =  "sol.png";
-                    }
-                    else if (item.summary.Contains("nublado"))
-                    {
-                        item.icon = "nublado.png";
-                    }
-                    else if (item.summary.Contains("chuva"))
-                    {
-                        item.icon = "chuva.png";
-                    }
-                    else
-                    {
-                        item.icon = "sol.png";
-                    }
-                }
-                
-            }
-            catch (Exception ex)
-            {
-                var a = ex.Message;
-            }
-           
-        }
-
+        /// <summary>
+        /// ApiDados
+        /// </summary>
+        /// <returns></returns>
         private async Task ApiDados()
         {
-             Tempo =  _ForecastIORequest.Get();
-              Currently= Tempo.currently;
-              Daily= Tempo.daily;
-              Minutely= Tempo.minutely;
-              Hourly = Tempo.hourly;
-
+            Tempo = _ForecastIORequest.Get();
+            Currently = Tempo.currently;
+            Daily = Tempo.daily;
 
             // Converter data
             Date = Extensions.ToDateTime(Currently.time);
-         
+
+            // Carregar Imagem
             LoadImageAnimada();
 
         }
-
+        /// <summary>
+        /// LoadImageAnimada
+        /// </summary>
         private void LoadImageAnimada()
         {
             // Pegar a Imagem Aminada de Acordo Com Summary
@@ -241,7 +155,88 @@ namespace TesteUol.ViewModels
             }
         }
 
+        #endregion
 
+        #region Carregar Lista e Geolocation
+        /// <summary>
+        /// /LoadListDailyForecas
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadListDailyForecas()
+        {
+            try
+            {
+                // Array.Remove(Daily.data, 0);
+                Daily.data.RemoveAt(0);
+                foreach (var item in Daily.data)
+                {
+                    item.date = Extensions.ToDateTime(item.time);
+                    item.celsiu = Extensions.ConvertCelsius(item.temperatureMax);
+
+                    // Pegar a Imagem de Acordo Com Summary
+                    // Explicando Para economizar tempo eu fiz deste jeito e logico que fosse MultiIdiomas faria de outra Maneira 
+                    if (item.summary.Contains("sol"))
+                    {
+                        item.icon = "sol.png";
+                    }
+                    else if (item.summary.Contains("nublado"))
+                    {
+                        item.icon = "nublado.png";
+                    }
+                    else if (item.summary.Contains("chuva"))
+                    {
+                        item.icon = "chuva.png";
+                    }
+                    else
+                    {
+                        item.icon = "sol.png";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var a = ex.Message;
+            }
+
+        }
+
+        /// <summary>
+        /// Pegar Geo Localização Testando 
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadGeolocation()
+        {
+            try
+            {
+                var localizacao = await Geolocation.GetLastKnownLocationAsync();
+                if (localizacao != null)
+                {
+                    LatitudeGeolocalizacao = localizacao.Latitude;
+                    LongitudeGeolocalizacao = localizacao.Longitude;
+
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Recurso não suportado no device
+                var g = ("Erro ", fnsEx.Message, "Ok");
+            }
+            catch (PermissionException pEx)
+            {
+                // Tratando erro de permissão
+                var g = ("Erro: ", pEx.Message, "Ok");
+            }
+            catch (Exception ex)
+            {
+                // Não foi possivel obter localização
+                var g = ("Erro : ", ex.Message, "Ok");
+            }
+
+        }
+        #endregion
+
+        #region Commands e Metedos
         private DelegateCommand<DailyForecast> _detailClimaCommand;         public DelegateCommand<DailyForecast> DetailsClimaCommand => _detailClimaCommand ?? (_detailClimaCommand = new DelegateCommand<DailyForecast>(async (item) => await DetailsClimaAsync(item)));
         /// <summary>
         /// DetailsClimaAsync
@@ -250,8 +245,8 @@ namespace TesteUol.ViewModels
         /// <returns></returns>
         private async Task DetailsClimaAsync(DailyForecast item)
         {
-           await PopupNavigation.Instance.PushAsync(new DetailsClimaPage(item));
-           
+            await PopupNavigation.Instance.PushAsync(new DetailsClimaPage(item));
+
         }
 
         private DelegateCommand<string> _mudarCidadeCommand;         public DelegateCommand<string> MudarCidadeCommand => _mudarCidadeCommand ?? (_mudarCidadeCommand = new DelegateCommand<string>(async (item) => await MudarCidadeAsync(item)));
@@ -262,7 +257,7 @@ namespace TesteUol.ViewModels
         /// <returns></returns>
         private async Task MudarCidadeAsync(string city)
         {
-             try
+            try
             {
                 if (city == null)
                 {
@@ -272,18 +267,14 @@ namespace TesteUol.ViewModels
                 // para Setar Api
                 // Devido a Falta de tempo Infelizmente não deu para Fazer melhor
                 //Porem queria mostra conhecimento que sei fazer. Pegar a Geocodificação 
-               // IsGeocodificacao = true;
+                // IsGeocodificacao = true;
                 var locations = await Geocoding.GetLocationsAsync(city);
                 var location = locations?.FirstOrDefault();
                 if (location != null)
                 {
                     LatitudeGeocodificacao = location.Latitude;
                     LongitudeGeolocalizacao = location.Longitude;
-                    await PopupNavigation.Instance.PushAsync(new CidadesPage(LatitudeGeocodificacao,LongitudeGeolocalizacao));
-
-                    //await LoadApi();
-                    //await ApiDados();
-                    //await LoadListDailyForecas();
+                    await PopupNavigation.Instance.PushAsync(new CidadesPage(LatitudeGeocodificacao, LongitudeGeolocalizacao));
                 }
 
             }
@@ -292,6 +283,8 @@ namespace TesteUol.ViewModels
                 var b = ex.Message;
             }
         }
+        #endregion
+
     }
 }
 
